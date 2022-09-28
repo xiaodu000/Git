1ddd9292f1bb609b4b455b5f17aca12e2ae685bf
xiaodu000 <114543269+xiaodu000@users.noreply.github.com>
2022-09-28 11:24:33 +0800

    Update GitTest.java

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello master2");
         System.out.println("hello hot-fix2");
+        System.out.println("poll test");
     }
 }
